f16de0ee631dfaaead7847965b242f4e1c1e5070
Attent1 <rafaeldsc22@outlook.com>
2024-08-05 09:11:20 -0300

    Configs

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Sphere</h1> 
+      <ModeToggle/>
+      <Button>Login</Button>
+    </main>
+  );
+}
